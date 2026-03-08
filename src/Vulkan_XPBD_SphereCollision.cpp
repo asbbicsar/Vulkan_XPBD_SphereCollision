fbb8b46265c7d27beda30cc63b5812051f43bbd3
asbbicsar <asbbicsar@konkuk.ac.kr>
2026-03-08 19:39:54 +0900

    Fixed shader names in cpp file

--- src/Vulkan_XPBD_SphereCollision.cpp
+++ src/Vulkan_XPBD_SphereCollision.cpp
@@ -784,7 +784,7 @@ private:
         }
     }
     void createComputePipeline() {
-        auto computeShaderCode = readFile("shaders/comp.spv"); // 컴파일된 스피어브이 파일
+        auto computeShaderCode = readFile("shaders/ComputeShader.comp.spv"); // 컴파일된 스피어브이 파일
         VkShaderModule computeShaderModule = createShaderModule(computeShaderCode);
 
         VkPipelineShaderStageCreateInfo shaderStageInfo{};
@@ -818,8 +818,8 @@ private:
         vkDestroyShaderModule(device, computeShaderModule, nullptr);
     }
     void createGraphicsPipeline() {
-        auto vertShaderCode = readFile("shaders/vert.spv");
-        auto fragShaderCode = readFile("shaders/frag.spv");
+        auto vertShaderCode = readFile("shaders/VertexShader.vert.spv");
+        auto fragShaderCode = readFile("shaders/FragmentShader.frag.spv");
 
         VkShaderModule vertShaderModule = createShaderModule(vertShaderCode);
         VkShaderModule fragShaderModule = createShaderModule(fragShaderCode);
